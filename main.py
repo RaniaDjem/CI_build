0d3831b1058b6ffa4c6235a58594f56a57a45215
RaniaDjem <85317648+RaniaDjem@users.noreply.github.com>
2024-02-22 17:36:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 import mysql.connector
 
-def return_basic(request):
+#def return_basic(request):
+def CIBUILD2(request):
   now = datetime.now()
   return '<h1>Welcomee to Cloud Functions and Cloud Build demo</h1><br/>' + str(now)
